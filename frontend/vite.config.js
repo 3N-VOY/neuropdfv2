22f98934ff121c35be2d92588da53121c6bd2e97
Alexander <xagorarisalexander@gmail.com>
2025-05-05 20:23:21 +0300

    fix config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: process.env.VITE_BASE_PATH || "/neuropdfv2",
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
-  build: {
-    rollupOptions: {
-      input: '/src/main.jsx'
-    }
-  }
 });
